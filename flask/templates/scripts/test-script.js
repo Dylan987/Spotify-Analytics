76a0667df0dd3418cae636c8a734ac77bce529e2
nobadinohz <nobadinohz@users.noreply.github.com>
2019-06-29 22:25:41 -0400

    player widget
    just need to fetch the id and dropdown value and it should work, in theory

--- flask/templates/scripts/test-script.js
+++ flask/templates/scripts/test-script.js
@@ -7,15 +7,22 @@ function handle_analysis_form(event) {
   var request = new XMLHttpRequest();
 
   request.onload = function() {
-    console.log(request.responseText);
+      console.log(request.responseText + "HELLO");
 
     var they-typed = document.createElement("p");
     they-typed.innerText = 'You Typed: ' + q;
     body.appendElement(they-typed);
 
+    //var iframe = document.getElementByTagName("iframe")[0];
+    //var att = document.createAttribute("src");
+    //att.value = "https://open.spotify.com/embed/track" + request.responseText[12];
+    //iframe.setAttributeNode(att);
+
     var their-analysis = document.createElement("p");
     their-analysis.innerText = "Your Analysis: " + request.responseText;
     body.appendElemenent(their-analysis);
+
+
   };
   request.onerror = function () {
     console.log(request.status);
