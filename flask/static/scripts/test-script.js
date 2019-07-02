ec31836e6328bb64e622d82efbadacb96e0a9bb4
Dylan Snelgrove <45444843+Dylan987@users.noreply.github.com>
2019-07-02 19:36:50 -0400

    Code Clean and delete evs work
    =(

--- flask/static/scripts/test-script.js
+++ flask/static/scripts/test-script.js
@@ -16,158 +16,136 @@ var handle_button = function(event) {
 
       number_of_displayed = Math.min(5, data[type + "s"]["items"].length);  // caps at 5 at
 
-      if (number_of_displayed == 0) {
-        var try_again = document.createElement("p");
-        try_again.appendChild(document.createTextNode("No results to display. Try another search."));
-        var row = document.createElement("div");
-        row.setAttribute("class", "result_row");
-        row.appendChild(try_again);
-        table_rows.appendChild(row);
-    } else {
       //delete any previous searches
       if (document.querySelector(".results_container") !== null) {
         console.log("deleting");
         document.body.removeChild(document.querySelector(".results_container"));
       }
 
-      var table_rows = document.createElement("div");
+      //add the new search
+      var table_rows = document.createElement("div"); //creating the container
       table_rows.setAttribute("class", "results_container");
-      if (type == "track") {
-        var items = data["tracks"]["items"];
-        //here we go
-        var analyses = [];
-        url2 = "/analysis/" + items[0]["id"];
-        for (var i = 1; i < number_of_displayed; i++){
-            url2 += "," + items[i]["id"];
-        }
-        var analysis_request = new XMLHttpRequest();
-        analysis_request.onload = function() {
-            features = JSON.parse(analysis_request.responseText)["audio_features"];
-            for (var analysis in features) {
-                analyses.push(features[analysis]);
-            }
-        }
-        analysis_request.onerror = function() {
-            console.log(request.status);
-        }
-        analysis_request.open("GET", url2, true);
-        analysis_request.send();
-        console.log(analyses);
-        //here we go
-        for (var i = 0; i < number_of_displayed; i++) {
 
-          var row = document.createElement("div");
-          row.setAttribute("class", "result_row");
-
-          var track_name = document.createElement("p");
-          track_name.appendChild(document.createTextNode("Name: " + items[i]["name"]));
-          row.appendChild(track_name);
-
-          var artist_name = document.createElement("p");
-          artist_name.appendChild(document.createTextNode("Artist: " + items[i]["artists"][0]["name"]));
-          row.appendChild(artist_name);
-
-          var album_name = document.createElement("p");
-          album_name.appendChild(document.createTextNode("Album: " + items[i]["album"]["name"]));
-          row.appendChild(album_name);
-
-          var the_analysis = document.createElement("ul");
-          the_analysis.appendChild(document.createTextNode("Analysis: "));
-          for (var feature in analyses[i]) {
-              var f = document.createElement("li");
-              console.log(feature);
-              f.appendChild(document.createTextNode(feature + ": " + analyses[i][feature]));
-              the_analysis.appendChild(f);
-          }
-          row.appendChild(the_analysis);
-
-          var preview_audio = document.createElement("iframe");
-          for (var key in iframe_rules) {
-              preview_audio.setAttribute(key, iframe_rules[key]);
-          }
-          preview_audio.setAttribute("src", "https://open.spotify.com/embed/" + type + "/" + items[i]["id"]);
-          row.appendChild(preview_audio);
-
-          table_rows.appendChild(row);
-        }
-        document.body.appendChild(table_rows);
-      }
-      else if (type == "artist") {
-        var items = data["artists"]["items"];
-        for (var i = 0; i < number_of_displayed; i++) {
-          var row = document.createElement("div");
-          row.setAttribute("class", "result_row");
-
-          var artist_name = document.createElement("p");
-          artist_name.appendChild(document.createTextNode("Name: " + items[i]["name"]));
-          row.appendChild(artist_name);
-
-          var preview_audio = document.createElement("iframe");
-          for(var key in iframe_rules) {
-              preview_audio.setAttribute(key, iframe_rules[key]);
-          }
-          preview_audio.setAttribute("height", "380");
-          preview_audio.setAttribute("src", "https://open.spotify.com/embed/" + type + "/" + items[i]["id"]);
-          row.appendChild(preview_audio);
-
-          table_rows.appendChild(row);
-        }
-        document.body.appendChild(table_rows);
-      }
-      else if (type == "album") {
-        var items = data["albums"]["items"];
-        for (var i = 0; i < number_of_displayed; i++) {
-          var row = document.createElement("div");
-          row.setAttribute("class", "result_row");
-
-          var album_name = document.createElement("p");
-          album_name.appendChild(document.createTextNode("Name: " + items[i]["name"]));
-          row.appendChild(album_name);
-
-          var artist_name = document.createElement("p");
-          artist_name.appendChild(document.createTextNode("Artist: " + items[i]["artists"][0]["name"]));
-          row.appendChild(artist_name);
-
-          //maybe a collapsible for track names + analysis darkSlateGray
-          var preview_audio = document.createElement("iframe");
-          for(var key in iframe_rules) {
-                  preview_audio.setAttribute(key, iframe_rules[key]);
-          }
-          preview_audio.setAttribute("height", "380")
-          preview_audio.setAttribute("src", "https://open.spotify.com/embed/" + type + "/" + items[i]["id"]);
-          row.appendChild(preview_audio);
-
-          table_rows.appendChild(row);
-        }
-        document.body.appendChild(table_rows);
-      }
-      else if (type == "playlist") {
-        var items = data["playlists"]["items"];
-        table_rows.setAttribute("class", "results_container");
-        for (var i = 0; i < number_of_displayed; i++) {
-          var row = document.createElement("div");
-          row.setAttribute("class", "result_row");
-
-          var playlist_name = document.createElement("p");
-          playlist_name.appendChild(document.createTextNode("Name: " + items[i]["name"]));
-          row.appendChild(playlist_name);
-
-          var preview_audio = document.createElement("iframe");
-          for(var key in iframe_rules){
-              preview_audio.setAttribute(key, iframe_rules[key]);
-          }
-          preview_audio.setAttribute("height", "380")
-          preview_audio.setAttribute("src", "https://open.spotify.com/embed/" + type + "/" + items[i]["id"]);
-          row.appendChild(preview_audio);
-
-          table_rows.appendChild(row);
-        }
-        document.body.appendChild(table_rows);
+      if (number_of_displayed == 0) { //if they send gibberish (or typos)
+        var try_again = document.createElement("p");
+        try_again.appendChild(document.createTextNode("No results to display. Try another search."));
+        var row = document.createElement("div");
+        row.setAttribute("class", "result_row");
+        row.appendChild(try_again);
+        table_rows.appendChild(row);
       }
       else {
-        console.log("What happened!?")
+        if (type == "track") {
+          //tracks
+          var items = data["tracks"]["items"]; //array of "track objects"
+          for (var i = 0; i < number_of_displayed; i++) {
+            //create a "row" - a display for a single item
+            var row = document.createElement("div");
+            row.setAttribute("class", "result_row");
+
+            var track_name = document.createElement("p");
+            track_name.appendChild(document.createTextNode("Name: " + items[i]["name"]));
+            row.appendChild(track_name);
+
+            var artist_name = document.createElement("p");
+            artist_name.appendChild(document.createTextNode("Artist: " + items[i]["artists"][0]["name"]));
+            row.appendChild(artist_name);
+
+            var album_name = document.createElement("p");
+            album_name.appendChild(document.createTextNode("Album: " + items[i]["album"]["name"]));
+            row.appendChild(album_name);
+
+            //embeded spotify play button (looks nice)
+            var preview_audio = document.createElement("iframe");
+            for (var key in iframe_rules) {
+                preview_audio.setAttribute(key, iframe_rules[key]);
+            }
+            preview_audio.setAttribute("src", "https://open.spotify.com/embed/" + type + "/" + items[i]["id"]);
+            row.appendChild(preview_audio);
+
+            table_rows.appendChild(row);
+          }
+        }
+        else if (type == "artist") {
+          var items = data["artists"]["items"]; //array of "artist" objects
+          for (var i = 0; i < number_of_displayed; i++) {
+            //create a "row" - a display for a single item
+            var row = document.createElement("div");
+            row.setAttribute("class", "result_row");
+
+            var artist_name = document.createElement("p");
+            artist_name.appendChild(document.createTextNode("Name: " + items[i]["name"]));
+            row.appendChild(artist_name);
+
+            //embeded spotify play button (looks nice)
+            var preview_audio = document.createElement("iframe");
+            for(var key in iframe_rules) {
+                preview_audio.setAttribute(key, iframe_rules[key]);
+            }
+            preview_audio.setAttribute("height", "380");
+            preview_audio.setAttribute("src", "https://open.spotify.com/embed/" + type + "/" + items[i]["id"]);
+            row.appendChild(preview_audio);
+
+            table_rows.appendChild(row);
+          }
+        }
+        else if (type == "album") {
+          var items = data["albums"]["items"]; //array of album objects
+          for (var i = 0; i < number_of_displayed; i++) {
+            //create a "row" - a display for a single item
+            var row = document.createElement("div");
+            row.setAttribute("class", "result_row");
+
+            var album_name = document.createElement("p");
+            album_name.appendChild(document.createTextNode("Name: " + items[i]["name"]));
+            row.appendChild(album_name);
+
+            var artist_name = document.createElement("p");
+            artist_name.appendChild(document.createTextNode("Artist: " + items[i]["artists"][0]["name"]));
+            row.appendChild(artist_name);
+
+            //embeded spotify play button (looks nice)
+            var preview_audio = document.createElement("iframe");
+            for(var key in iframe_rules) {
+                    preview_audio.setAttribute(key, iframe_rules[key]);
+            }
+            preview_audio.setAttribute("height", "380")
+            preview_audio.setAttribute("src", "https://open.spotify.com/embed/" + type + "/" + items[i]["id"]);
+            row.appendChild(preview_audio);
+
+            table_rows.appendChild(row);
+          }
+        }
+        else if (type == "playlist") {
+          var items = data["playlists"]["items"]; //aray of "playlist" objects
+          table_rows.setAttribute("class", "results_container");
+          for (var i = 0; i < number_of_displayed; i++) {
+            //create a "row" - a display for a single item
+            var row = document.createElement("div");
+            row.setAttribute("class", "result_row");
+
+            var playlist_name = document.createElement("p");
+            playlist_name.appendChild(document.createTextNode("Name: " + items[i]["name"]));
+            row.appendChild(playlist_name);
+
+            //embeded spotify play button (looks nice)
+            var preview_audio = document.createElement("iframe");
+            for(var key in iframe_rules){
+                preview_audio.setAttribute(key, iframe_rules[key]);
+            }
+            preview_audio.setAttribute("height", "380")
+            preview_audio.setAttribute("src", "https://open.spotify.com/embed/" + type + "/" + items[i]["id"]);
+            row.appendChild(preview_audio);
+
+            table_rows.appendChild(row);
+          }
+        }
+        else {
+          console.log("What happened!?")
+        }
       }
-    }
+      //add the finished search to the document
+      document.body.appendChild(table_rows);
     };
     request.onerror = function () {
       console.log(request.status);
