30fb712788945352e84e3c6dab837f8614ec44c7
Li-Evelyn <lievelynpl@gmail.com>
2019-07-04 16:21:41 -0400

    html documents are significantly shorter
    content's all still there, don't worry
    also working on rendering the analysis results page

--- flask/hello.py
+++ flask/hello.py
@@ -26,23 +26,24 @@ def analysis(q):
 
 @app.route("/analysis.html")
 def analysis_page():
-    return render_template("analysis.html")
+    return render_template("analysis.html", title="Spotify Analytics - Analysis", big_header="Analytics")
 
 @app.route("/home.html")
 def home_page():
-    return render_template("home.html")
+    return render_template("home.html", title="Spotify Analytics", big_header="Spotify Analytics")
 
 @app.route("/about.html")
 def about_page():
-    return render_template("about.html")
+    return render_template("about.html", title="Spotify Analytics - About", big_header="About")
 
 @app.route("/contact.html")
 def contact_page():
-    return render_template("contact.html")
+    return render_template("contact.html", title="Spotify Analytics - Contact", big_header="Contact")
+
 @app.route("/playlist-generator.html")
 def generator_page():
-    return render_template("playlist-generator.html")
+    return render_template("playlist-generator.html", title="Spotify Analytics - Playlist Generator", big_header="Generate Playlist")
 
 @app.route("/login.html")
 def login_page():
-    return render_template("login.html")
+    return render_template("login.html", title="Spotify-Analytics - User Authorization", big_header="pls authorize")
