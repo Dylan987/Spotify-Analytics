4a30d3a3ce8072eaab2425236918aeab54096c61
Dylan Snelgrove <45444843+Dylan987@users.noreply.github.com>
2019-09-21 14:27:49 -0400

    work (in progress) to redirect page

---
+++ flask/static/scripts/redirect_script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  //get the token
+  let hash = location.hash.slice(1);
+  let params = new URLSearchParams(hash);
+  let token = params.get("access_token");
+  console.log(token);
+
+  //use the token to make the request
+  
+
+
+});
